262d0f98aaf4d36d21d4647bbfa979916259d909
Alan Taranti <alan.taranti@gmail.com>
2018-11-27 12:16:10 -0300

    adicionado lampada na home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,38 @@
 <template>
+    <v-container fluid>
+        <template v-for="lampada, index in lampadas">
+            <lampada :dispositivo="lampada" :pseudoid="index +1"></lampada>
+        </template>
+    </v-container>
 </template>
 
 <script>
 
+    import Lampada from '../components/Lampada'
+
     export default {
-        components: {}
+        components: {Lampada},
+        data: () => ({
+            lampadas: [
+                {
+                    id: 122,
+                    versao: 1,
+                    codigo: 11,
+                    estado: 0,
+                    leitura_valor: null,
+                    leitura_unidade: "",
+                    sensibilidade: null,
+                },
+                {
+                    id: 213,
+                    versao: 1,
+                    codigo: 11,
+                    estado: 1,
+                    leitura_valor: null,
+                    leitura_unidade: "",
+                    sensibilidade: null,
+                }
+            ]
+        }),
     }
 </script>
