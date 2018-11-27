df0e22131b759244abc6fcaf04d21f0f77961416
Alan Taranti <alan.taranti@gmail.com>
2018-11-27 12:15:52 -0300

    adicionado rota de configuração

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from './views/Home'
+import Settings from './views/Settings'
 import Estrutura from './views/Estrutura'
 import Login from './views/Login'
 
@@ -40,6 +41,11 @@ const router = new Router({
                     path: '/home',
                     name: 'home',
                     component: Home,
+                },
+                {
+                    path: '/configuracoes',
+                    name: 'settings',
+                    component: Settings,
                 },
             ],
         },
