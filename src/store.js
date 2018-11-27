296bc60e28c70d25426ebe67d64b76d40dc2c6d1
Alan Taranti <alan.taranti@gmail.com>
2018-11-27 10:18:31 -0300

    adicionado estados em vuex

--- src/store.js
+++ src/store.js
@@ -1,16 +1,64 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+/* eslint-disable no-param-reassign */
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
+const store = new Vuex.Store({
+    state: {
+        navigation: {
+            drawerMostrar: false
+        },
+        layout: {
+            drawerBreakpointEmTelaGrande: 1264
+        },
+        toolbar: {
+            toolbarTitulo: 'ResAuto',
+            toolbarIconeEsquerdaClicado: false
+        },
+        toaster: {
+            toasterMostrar: false,
+            toasterMensagem: '',
+            toasterColor: 'primary',
+        }
+    },
 
-  },
-  mutations: {
+    getters: {
+        drawerMostrar: state => state.navigation.drawerMostrar,
+        drawerBreakpointEmTelaGrande: state => state.layout.drawerBreakpointEmTelaGrande,
+        toolbarIconeEsquerdaClicado: state => state.toolbar.toolbarIconeEsquerdaClicado,
+        toolbarTitulo: state => state.toolbar.toolbarTitulo,
+        toasterMostrar: state => state.toaster.toasterMostrar,
+        toasterMensagem: state => state.toaster.toasterMensagem,
+        toasterColor: state => state.toaster.toasterColor,
+    },
 
-  },
-  actions: {
+    mutations: {
+        drawerMostrar: (state, drawerMostrar) => {
+            state.navigation.drawerMostrar = drawerMostrar;
+        },
+        drawerAlternar: (state) => {
+            state.navigation.drawerMostrar = !state.navigation.drawerMostrar;
+        },
+        drawerBreakpointEmTelaGrande: (state, drawerBreakpointEmTelaGrande) => {
+            state.layout.drawerBreakpointEmTelaGrande = drawerBreakpointEmTelaGrande;
+        },
+        toolbarIconeEsquerdaClicado: (state, toolbarIconeEsquerdaClicado) => {
+            state.toolbar.toolbarIconeEsquerdaClicado = toolbarIconeEsquerdaClicado;
+        },
+        toolbarTitulo: (state, toolbarTitulo) => {
+            state.toolbar.toolbarTitulo = toolbarTitulo;
+        },
+        toasterMostrar: (state, toasterMostrar) => {
+            state.toaster.toasterMostrar = toasterMostrar;
+        },
+        toasterMensagem: (state, toasterMensagem) => {
+            state.toaster.toasterMensagem = toasterMensagem;
+        },
+        toasterColor: (state, toasterColor) => {
+            state.toaster.toasterColor = toasterColor;
+        },
+    },
+});
 
-  }
-})
+export default store;
