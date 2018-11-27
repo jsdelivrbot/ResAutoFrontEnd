874603d3fff968e034fc6a9f0fb6c6a79e553a7a
Alan Taranti <alan.taranti@gmail.com>
2018-11-27 10:18:08 -0300

    adicionado tema

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,11 +2,25 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import pt from 'vuetify/es5/locale/pt'
+import colors from 'vuetify/es5/util/colors'
+
 
 Vue.use(Vuetify, {
-  iconfont: 'fa',
-  lang: {
-    locales: { pt },
-    current: 'pt'
-  },
-})
+    iconfont: 'fa',
+    theme: {
+        primary: colors.red.darken1,
+        secondary: colors.cyan,
+        accent: colors.cyan,
+
+        background: '#FFFAFA',
+
+        error: colors.red.lighten1,
+        info: colors.lightBlue.lighten1,
+        success: colors.lightGreen.darken3,
+        warning: colors.yellow.darken1,
+    },
+    lang: {
+        locales: {pt},
+        current: 'pt'
+    },
+});
