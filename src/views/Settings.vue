c4bbddf51e67c48745699561cc544ebe8a051148
Alan Taranti <alan.taranti@gmail.com>
2018-11-27 12:14:45 -0300

    adicionado view de configurações

---
+++ src/views/Settings.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-container fluid>
+        <v-card flat color="transparent" style="margin-top: 30px;">
+            <v-card-title primary-title style="padding: 0;">
+                <v-layout align-center column fill-height>
+                    <div class="xs-12">
+                        <div class="headline text-xs-center grey--text text--darken-3"
+                             style="text-shadow: 1px 1px #000;">Sensor de Luminosidade <br> (Lux)
+                        </div>
+                    </div>
+                    <div class="xs-12">
+                        <div style="align-items: center; display: flex; justify-content: center">
+                            <v-slider
+                                    v-model="sensorLuminosidade"
+                                    thumb-label
+                                    max="100"
+                                    min="0"
+                                    @change="sensibilidadeLuminosidadeEscolhida()"
+                            ></v-slider>
+                        </div>
+                    </div>
+                </v-layout>
+            </v-card-title>
+        </v-card>
+
+        <v-card flat color="transparent" style="margin-top: 30px;">
+            <v-card-title primary-title style="padding: 0;">
+                <v-layout align-center column fill-height>
+                    <div class="xs-12">
+                        <div class="headline text-xs-center grey--text text--darken-3"
+                             style="text-shadow: 1px 1px #000;">Sensor de Presença
+                        </div>
+                    </div>
+                    <div class="xs-12">
+                        <div style="align-items: center; display: flex; justify-content: center">
+                            <v-switch color="green" v-model="sensorPresencaEstado"></v-switch>
+                        </div>
+                    </div>
+                </v-layout>
+            </v-card-title>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+
+    export default {
+        components: {},
+        data: () => ({
+            sensorPresencaId: null,
+            sensorPresencaEstado: false,
+            sensorLuminosidadeId: null,
+            sensorLuminosidade: 0,
+        }),
+        methods: {
+            sensibilidadeLuminosidadeEscolhida() {
+                // TODO: notificar backend alteração em sensor de luminosidade
+                console.log('sensorLuminosidade', this.sensorLuminosidade);
+            }
+        },
+        watch: {
+            sensorPresencaEstado() {
+                // TODO: notificar backend alteração em sensor de presença
+                console.log('sensorPresencaEstado', this.sensorPresencaEstado);
+            },
+        },
+        mounted() {
+            // TODO: alterar sensor de presença e luminosidade de acordo com backend
+            this.sensorPresencaEstado = false;
+            this.sensorLuminosidade = 0;
+        }
+    }
+</script>
